205d8cbdb3445da2394b61f116dee3c6b7c6c75b
Abenezer0923 <abnew0923@gmail.com>
2024-03-06 16:16:54 +0300

    Redux add

---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+// export const BASE_URL =
+//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+export const BASE_URL = ''; // If using proxy
+export const PRODUCTS_URL = 'http://localhost:2025/api/products';
+export const USERS_URL = 'http://localhost:2025/api/users';
+export const ORDERS_URL = 'http://localhost:2025/api/orders';
+export const PAYPAL_URL = 'http://localhost:2025/api/config/paypal';
